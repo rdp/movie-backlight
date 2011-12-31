7f85e5b86a9e06c628a4b3987a8893e45e757df7
rogerdpack <rogerpack2005@gmail.com>
2011-12-30 19:28:14 -0700

    ts d

--- raw/go_upscaling.bat
+++ raw/go_upscaling.bat
@@ -1,1 +1,2 @@
-mplayer -nocache  -aspect 1920:1080 upconvert_from_screen_me2.avs
+mplayer -nocache -vf mirror,scale=1600  -aspect 1920:1080 upconvert_from_screen_me2.avs
+@rem 1600 is the smallest monitor, where it happens to start [mplayer bug?]
